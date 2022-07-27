bec56caf200dccc813d4ac4766ede55c167343ef
Will Sutton <wsutton17@gmail.com>
2022-07-27 17:53:20 -0400

    formatting diff

--- crons.py
+++ crons.py
@@ -3,7 +3,7 @@ from jobs.a import job_a
 from jobs.b import job_b
 
 def logger(msg):
-    print(f"""<<====={dt.datetime.now().ctime()[3:-1]}====>>>>> {msg}""")
+    print(f"""<<====={dt.datetime.now().ctime().split(" ")[3:-1][0]}====>>>>> {msg}""")
 
 def run_job_a():
     ret = job_a()
